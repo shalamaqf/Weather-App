1b972d59c946390bc5deba08dac7a62abd843892
Shalama <shalamafadillaa@gmail.com>
2025-12-03 10:43:50 +0700

    Create a function to get location JSON data
    Store the location input and fetch the location data from the API's.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ function getLocation() {
     console.log(location);
     return location;
 }
+
+/* Create an async function to receive the location input
+   and fetch the location JSON data */
+async function getLocationData() {
+    const key = '2F8ZWAB7SDMAQ7TREVK5VY772';
+    try {
+        const location = getLocation();
+        const locationData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`);
+        return locationData;
+    } catch {
+        console.log(err);
+    }
+}
