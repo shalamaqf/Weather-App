876d1fe7b9d9d0c4ed99fe0387d1a1c36d6b714d
Shalama <shalamafadillaa@gmail.com>
2025-12-03 10:19:33 +0700

    Create a function to set form and get location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+
+// Create a function to set up the form and get the location
+function getLocation() {
+    const location = prompt('Enter a location:');
+    console.log(location);
+    return location;
+}
