ba681089e0e4d589fb23734c604016ee6845b20e
Shalama <shalamafadillaa@gmail.com>
2025-12-03 14:01:20 +0700

    Call turnDataToObject function inside an event

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,9 +1,9 @@
-import { getLocationData } from '../logic/logic.js';
+import { turnDataToObject } from '../logic/logic.js';
 
 // Create a function to attach an event listener on search button
 export function attachEventSearchButton() {
     const searchButton = document.getElementById('search');
     searchButton.addEventListener('click', () => {
-        getLocationData();
+        turnDataToObject();
     })
 }
--- src/logic/logic.js
+++ src/logic/logic.js
@@ -1,18 +1,16 @@
 // Create a function to set up the form and get the location
 function getLocation() {
     const location = document.getElementById('location').value;
-    console.log(location);
     return location;
 }
 
 /* Create an async function to receive the location input
    and fetch the location JSON data */
-export async function getLocationData() {
+async function getLocationData() {
     const key = '2F8ZWAB7SDMAQ7TREVK5VY772';
     try {
         const location = getLocation();
         const locationData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`);
-        console.log(locationData);
         return locationData;
     } catch {
         console.log(err);
@@ -20,7 +18,7 @@ export async function getLocationData() {
 }
 
 // Create a function to turn JSON data to JS object
-async function turnDataToObject() {
+export async function turnDataToObject() {
     try {
         const locationData = await getLocationData();
         const data = await locationData.json();
