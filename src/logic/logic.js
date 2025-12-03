d4c8f7ba621c648341f8c7425f49d8711710c28f
Shalama <shalamafadillaa@gmail.com>
2025-12-03 13:56:25 +0700

    Get the input element's value

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -1,17 +1,18 @@
 // Create a function to set up the form and get the location
-export function getLocation() {
-    const location = document.getElementById('location');
+function getLocation() {
+    const location = document.getElementById('location').value;
     console.log(location);
     return location;
 }
 
 /* Create an async function to receive the location input
    and fetch the location JSON data */
-async function getLocationData() {
+export async function getLocationData() {
     const key = '2F8ZWAB7SDMAQ7TREVK5VY772';
     try {
         const location = getLocation();
         const locationData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`);
+        console.log(locationData);
         return locationData;
     } catch {
         console.log(err);
