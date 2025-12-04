dce789f59fa955a5bd5cd78503469ee28a50ff64
Shalama <shalamafadillaa@gmail.com>
2025-12-04 10:58:05 +0700

    Add an async keyword
    Add it on search button event listener for better UI and multiple async tasks work in order.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -3,9 +3,13 @@ import { getInfo, getGif } from '../logic/logic.js';
 // Create a function to attach an event listener on search button
 export function attachEventSearchButton() {
     const searchButton = document.getElementById('search');
-    searchButton.addEventListener('click', () => {
-        showTemps();
-        getGif();
+    searchButton.addEventListener('click', async () => {
+        try {
+            await showTemps();
+            await getGif();
+        } catch(err) {
+            console.log(err);
+        }
     })
 }
 
