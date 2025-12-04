85eb3d13e7bb486e56c42d7ac6fb5adc7d86296d
Shalama <shalamafadillaa@gmail.com>
2025-12-04 10:36:14 +0700

    Call getInfo and getGif in search button's event

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1,9 +1,10 @@
-import { turnDataToObject } from '../logic/logic.js';
+import { getInfo, getGif } from '../logic/logic.js';
 
 // Create a function to attach an event listener on search button
 export function attachEventSearchButton() {
     const searchButton = document.getElementById('search');
     searchButton.addEventListener('click', () => {
-        turnDataToObject();
+        getInfo();
+        getGif();
     })
 }
