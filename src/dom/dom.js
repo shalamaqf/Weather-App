2f198c441a89dedb2918183360fded88a8e8dac7
Shalama <shalamafadillaa@gmail.com>
2025-12-04 10:51:39 +0700

    Create a function to show the temps to screen

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -4,7 +4,22 @@ import { getInfo, getGif } from '../logic/logic.js';
 export function attachEventSearchButton() {
     const searchButton = document.getElementById('search');
     searchButton.addEventListener('click', () => {
-        getInfo();
+        showTemps();
         getGif();
     })
+}
+
+// Create a function to show the celcius and fahrenheit data
+async function showTemps() {
+    // Select elements
+    const celcius = document.querySelector('.data.celcius');
+    const fahrenheit = document.querySelector('.data.fahrenheit');
+
+    try {
+        const data = await getInfo();
+        celcius.textContent = data.celcius + '°C';
+        fahrenheit.textContent = data.fahrenheit + '°F';
+    } catch (err) {
+        console.log(err);
+    }
 }
